e1842dfc06ab9cae3affec18bd53de3048dcf052
chandan <chandansamantaray784@gmail.com>
2025-05-06 11:41:19 +0530

    data type and variable

---
+++ index.js
@@ -0,0 +1,29 @@
+//data type and variable
+
+// let student_age = 10.2; //4bytes
+
+// let name = "somya"; //10 bytes
+
+// let isBig = false;
+
+// console.log(typeof isBig);
+
+// let x = null;
+
+// console.log(typeof arr);
+
+//array  ,// object
+
+// let student1 = "chandan";
+// let student2 = "sonu";
+
+// let students = ["chandan","sonu"]
+
+// let student = {
+//   name: "csngfs",
+//   age: 1176,
+//   roll_no: 100,
+// };
+
+// let x = 20;
+// let X = 20;
