42a7481d61d659f2bc448a2daf195f3f1bc67dac
chandan <chandansamantaray784@gmail.com>
2025-05-06 13:01:32 +0530

    operator and conditional

---
+++ conditional.js
@@ -0,0 +1,64 @@
+// if
+// if else
+// if else lader
+
+// switch case
+
+// let isCloudy = false;
+// if (isCloudy) {
+//   console.log("wither is cloudy");
+// } else {
+//   console.log("wether is not cloudy");
+// }
+
+// let fullMark = 600;
+
+// let mark = 550;
+
+// if (mark > fullMark) {
+//   console.log("inavlid mark");
+// } else if (mark >= 500) {
+//   console.log("you got A grade");
+// } else if (mark >= 300) {
+//   console.log("you got B grades");
+// } else {
+//   console.log("you got C grade");
+// }
+
+let day = 1;
+
+let x = 2;
+
+switch (day) {
+  case "snghsghsgdv": {
+    console.log("Monday");
+    break;
+  }
+  case 1: {
+    console.log("tue");
+    break;
+  }
+  case 2: {
+    console.log("wed");
+    break;
+  }
+  case 3: {
+    console.log("thur");
+    break;
+  }
+  case 4: {
+    console.log("fri");
+    break;
+  }
+  case 5: {
+    console.log("sat");
+    break;
+  }
+  case 6: {
+    console.log("sun");
+    break;
+  }
+  default: {
+    console.log("invalid day numbers");
+  }
+}
---
+++ operator.js
@@ -0,0 +1,56 @@
+// 1- arithmatic opearator
+
+// let x = 5;
+
+// let y = 100;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+//realatsional
+
+// let x = 20;
+
+// let y = "20";
+
+// console.log(x + y);
+
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x == y);
+// console.log(x != y);
+
+// console.log(x === y);
+
+// let arr = [1,3,4,6]
+// let arr2 = arr
+
+// // stack = arr(101) , arr2(101)  heap= [1,3,4,6](101)
+
+//  assignment opearator
+
+// let x = 2;
+// x += 2;
+// console.log(x);
+// console.log(x);
+// console.log(x--);
+// console.log(x);
+
+//logical opearator
+
+// let x = 10;
+
+// console.log(x > 5 && x > 4 && x > 3);
+
+// console.log(x > 5 || x > 4 || x < 3);
+
+// and
+
+// let age = 30;
+
+// age > 18 ? console.log("you can vote") : console.log("you can not vote");
