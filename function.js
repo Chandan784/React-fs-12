2ab54e754ff5143228ccd6b7f4ab082f92f8bddb
chandan <chandansamantaray784@gmail.com>
2025-05-12 13:13:36 +0530

    array basics

--- function.js
+++ function.js
@@ -30,4 +30,43 @@
 // let ans = x(10, 20);
 
 // console.log(ans);
-console.log(window);
+// console.log(window);
+
+//function expresion
+
+//  let myFun=  function (x){
+
+// }
+
+//arrow function
+// let myFun = ()=>{}
+
+// function greet() {
+//   console.log("hello world");
+// }
+
+// let greet = () => {
+//   console.log("hello world");
+// };
+
+// greet();
+
+// function addTwoNum(x, y) {
+//   console.log(x + y);
+// }
+
+// let addTwoNum = (x, y) => {
+//   console.log(x + y);
+// };
+
+// addTwoNum(10, 30);
+
+// function addTwoNum(num1, num2) {
+//   return num1 + num2;
+// }
+
+// let addTwoNum = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// let addTwoNum = (num1,num2)=>num1+num2
---
+++ myArray.js
@@ -0,0 +1,58 @@
+// let student1 = "sonu";
+// let student2 = "soumya";
+// let student3 = "abhisek";
+
+// let students = ["sonu", "soumya", "abhisek"];
+
+// let nums = [1, 2, 3, 5, 6, 3, 4, 4, 4];
+
+// console.log(nums.length - 1);
+
+//get value by index
+// console.log(students[5]);
+//set value by index
+// students[1] = "chandan";
+
+// console.log(students);
+
+// stack = students(10) , heap = ["sonu","soumya","abhisek"](10)
+
+// console.log(students.length);
+
+// for (let index = 0; index <= students.length - 1; index++) {
+//   console.log(students[index]);
+// }
+
+// let students = ["sonu", "soumya", "abhisek"];
+
+// for (stu of students) {
+//   console.log(stu);
+// }
+
+// for (index in students) {
+//   console.log(index, students[index]);
+// }
+
+//sum elements of an array
+
+// let nums = [2, 10, 4, 9]; //25
+
+// function sumArray(arr) {
+//   let result = 0;
+
+//   for (let index = 0; index < arr.length; index++) {
+//     result = result + arr[index];
+//   }
+
+//   return result;
+// }
+
+// console.log(sumArray(nums));
+
+// arr= [2,10,4,9]
+//result = 25
+//index= 4
+
+//swap index of array
+
+// let nums = [1,2,3,4]  = [2,1,3,4]
